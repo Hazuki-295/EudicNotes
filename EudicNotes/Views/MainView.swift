ec7f1609c896a3125e937802af36704b3ce19b36
Hazuki-295 <luosheng233@outlook.com>
2024-05-04 12:05:50 +0800

    Update invisibleTemplate

--- EudicNotes/Views/MainView.swift
+++ EudicNotes/Views/MainView.swift
@@ -128,7 +128,9 @@ struct MainView: View {
 }
 
 struct Utils {
-    static private let invisibleTemplate = "<span style=\"opacity: 0; position: absolute;\">%@</span>"
+    static private func invisibleTemplate(_ firstChar: Character, middleString: String, _ lastChar: Character) -> String {
+        return "<span style=\"opacity: 0; position: absolute;\">\(firstChar)</span>" + "\(middleString)" + "<span style=\"opacity: 0; position: absolute;\">\(lastChar)</span>"
+    }
     
     // Generalized function to replace patterns in a string
     static private func replacePattern(in input: String, withRegexPattern regexPattern: String,
@@ -177,34 +179,34 @@ struct Utils {
     static private func replacePlusSign(_ input: String) -> String {
         let pattern = #"\+([^+]*)\+"#
         let baseTemplate = "<span style=\"color: #35A3FF; font-weight: bold;\">$1</span>" // light blue
-        let template = String(format: invisibleTemplate, "+") + baseTemplate + String(format: invisibleTemplate, "+")
+        let template = invisibleTemplate("+", middleString: baseTemplate, "+")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template)
     }
     
     static private func replaceSquareBrackets(_ input: String) -> String {
         let pattern = #"\[([^]]*)\]"#
         let baseTemplate = "<span style=\"color: #67A78A; font-weight: bold;\">$1</span>" // green
-        let template = String(format: invisibleTemplate, "[") + baseTemplate + String(format: invisibleTemplate, "]")
+        let template = invisibleTemplate("[", middleString: baseTemplate, "]")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template)
     }
     
     static private func replaceAngleBrackets(_ input: String) -> String {
         let pattern = "<([^>]*)>"
         let baseTemplate = "<span style=\"color: #F51225; font-weight: bold\">$1</span>" // red
-        let template = String(format: invisibleTemplate, "<") + baseTemplate + String(format: invisibleTemplate, ">")
+        let template = invisibleTemplate("<", middleString: baseTemplate, ">")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template)
     }
     
     // 2. LDOCE Style
     static private func replacePOS(_ input: String) -> String { // special style, dark red
-        let patternTemplatePairs: [String: String] = [
+        let replacements: [String: String] = [
             #"\b(noun|verb|adjective|adverb|preposition|conjunction|pronoun)\b"#: "<span style=\"font-family: Georgia; color: rgba(196, 21, 27, 0.8); font-size: 85%; font-weight: bold; font-style: italic; margin: 0 2px;\">$1</span>",
             #"\b(Phrasal Verb)\b"#: "<span style=\"font-family: Optima; color: rgba(196, 21, 27, 0.8); font-size: 74%; border: 1px solid rgba(196, 21, 27, 0.8); padding: 0px 3px; margin: 0 2px;\">$1</span>",
             #"\b(Idioms)\b"#: "<span style=\"font-family: Optima; color: #0072CF; font-size: 15px; font-weight: 600; word-spacing: 0.1rem; margin: 0 2px;\">$1</span>"
         ]
         
         var modifiedInput = input
-        for (pattern, template) in patternTemplatePairs {
+        for (pattern, template) in replacements {
             modifiedInput = replacePattern(in: modifiedInput, withRegexPattern: pattern, usingTemplate: template)
         }
         return modifiedInput
@@ -220,7 +222,8 @@ struct Utils {
     static private func replaceAsterisk(_ input: String) -> String { // special style, light blue with mark
         let pattern = #"\*([^*]*)\*"#
         let baseTemplate = "<span style=\"font-family: Optima; color: #0072CF; font-size: 15px; font-weight: 600; word-spacing: 0.1rem; background: linear-gradient(to bottom, rgba(0, 114, 207, 0) 55%, rgba(0, 114, 207, 0.15) 55%, rgba(0, 114, 207, 0.15) 100%); margin: 0 2px; padding-right: 3.75px\">$1</span>"
-        let template = String(format: invisibleTemplate, "*") + baseTemplate + String(format: invisibleTemplate, "*")
+        let template = invisibleTemplate("*", middleString: baseTemplate, "*")
+        
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template, transform: { match in
             var modifiedMatch = match
                 .replacingOccurrences(of: ",", with: "<span style=\"color: #DE002D;\">,</span>")
@@ -239,47 +242,47 @@ struct Utils {
     static private func replaceExclamation(_ input: String) -> String { // special style, tag
         let pattern = #"\!([^!]*)\!"#
         let baseTemplate = "<span style=\"font-family: Optima; color: white; font-size: 15px; font-weight: 600; font-variant: small-caps; background: #0072CF; border-radius: 4px 0 0 4px; display: inline-block; height: 16px; line-height: 15px; margin-right: 5px; padding: 0 2px 0 5px; position: relative; transform: translateY(-1px);\">$1<span style=\"width: 0; height: 0; position: absolute; top: 0; left: 100%; border-style: solid; border-width: 8px 0 8px 6px; border-color: transparent transparent transparent #0072CF;\"></span></span>"
-        let template = String(format: invisibleTemplate, "!") + baseTemplate + String(format: invisibleTemplate, "!")
+        let template = invisibleTemplate("!", middleString: baseTemplate, "!")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template)
     }
     
     static private func replaceAtSign(_ input: String) -> String { // light blue without mark
         let pattern = "@([^@]*)@"
         let baseTemplate = "<span style=\"font-family: Bookerly; color: #0072CF; font-size: 15px; word-spacing: 0.1rem;\">$1</span>"
-        let template = String(format: invisibleTemplate, "@") + baseTemplate + String(format: invisibleTemplate, "@")
+        let template = invisibleTemplate("@", middleString: baseTemplate, "@")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template, transform: { match in
-            let modifiedMatch = match
+            var modifiedMatch = match
                 .replacingOccurrences(of: "{,}", with: "<span style=\"color: #DE002D;\">,</span>")
                 .replacingOccurrences(of: "|", with: "<span style=\"color: #DE002D;\">|</span>")
             
-            let pattern = #"⟨([^⟩]*)⟩"#
-            let template = "<span style=\"font-size: 13.5px;\">$0</span>" // smaller
+            let replacements: [String: String] = [
+                "⟨([^⟩]*)⟩": "<span style=\"font-size: 13.5px;\">$0</span>", // smaller
+                #"\{([^}]*)\}"#: invisibleTemplate("{", middleString: "<span style=\"font-style: italic;\">$1</span>", "}") // italic
+            ]
+            for (pattern, template) in replacements {
+                modifiedMatch = replacePattern(in: modifiedMatch, withRegexPattern: pattern, usingTemplate: template)
+            }
             
-            let regex = try! NSRegularExpression(pattern: pattern, options: [])
-            let range = NSRange(match.startIndex..<match.endIndex, in: match)
-            return regex.stringByReplacingMatches(in: modifiedMatch, options: [], range: range, withTemplate: template)
+            return modifiedMatch
         })
     }
     
     static private func replaceAndSign(_ input: String) -> String { // black Bookerly
         let pattern = "&([^&]*)&"
         let baseTemplate = "<span style=\"font-family: Bookerly; font-size: 15px; word-spacing: 0.1rem;\">$1</span>"
-        let template = String(format: invisibleTemplate, "&") + baseTemplate + String(format: invisibleTemplate, "&")
+        let template = invisibleTemplate("&", middleString: baseTemplate, "&")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template, transform: { match in
             let pattern = #"\{([^}]*)\}"#
             let baseTemplate = "<span style=\"color: #007A6C; font-style: italic;\">$1</span>" // light green, italic
-            let template = String(format: invisibleTemplate, "{") + baseTemplate + String(format: invisibleTemplate, "}")
-            
-            let regex = try! NSRegularExpression(pattern: pattern, options: [])
-            let range = NSRange(match.startIndex..<match.endIndex, in: match)
-            return regex.stringByReplacingMatches(in: match, options: [], range: range, withTemplate: template)
+            let template = invisibleTemplate("{", middleString: baseTemplate, "}")
+            return replacePattern(in: match, withRegexPattern: pattern, usingTemplate: template)
         })
     }
     
     static private func replaceCaretSign(_ input: String) -> String { // light green, chinese
         let pattern = #"\^([^^]*)\^"#
         let baseTemplate = "<span style=\"font-family: 'Source Han Serif CN'; color: #007A6C; font-size: 13.5px; word-spacing: 0.1rem; padding: 0 2px; margin-left: 2px; background: rgba(0, 122, 108, 0.2); border-radius: 3px;\">$1</span>"
-        let template = String(format: invisibleTemplate, "^") + baseTemplate + String(format: invisibleTemplate, "^")
+        let template = invisibleTemplate("^", middleString: baseTemplate, "^")
         return replacePattern(in: input, withRegexPattern: pattern, usingTemplate: template)
     }
     
