af98dda382c2b89ec2527cd581ab9f85e6e9f5e4
Hazuki-295 <luosheng233@outlook.com>
2024-05-08 16:10:08 +0800

    Update OptionsWindowController.swift

--- EudicNotes/ViewControllers/OptionsWindowController.swift
+++ EudicNotes/ViewControllers/OptionsWindowController.swift
@@ -12,27 +12,18 @@ class OptionsWindowController: NSObject, NSWindowDelegate {
     private var optionsWindow: NSWindow?
     
     func openOptionsWindow(sharedNoteData: NoteData) {
-        if let existingWindow = optionsWindow {
-            existingWindow.makeKeyAndOrderFront(nil) // Bring the existing window to the front
-        } else {
-            let optionsView = OptionsView().environmentObject(sharedNoteData) // SwiftUI view
+        if optionsWindow == nil {
+            let optionsView = OptionsView().environmentObject(sharedNoteData)
             let hostingController = NSHostingController(rootView: optionsView)
-            
-            // Create the window with the hosting controller
-            let window = NSWindow(contentViewController: hostingController)
-            window.makeKeyAndOrderFront(nil)
-            window.title = "Options"
-            
-            // Keep a reference to the newly created window
-            self.optionsWindow = window
-            
-            // Set up for window close detection
-            window.isReleasedWhenClosed = false
-            window.delegate = self
+            optionsWindow = NSWindow(contentViewController: hostingController)
+            optionsWindow?.title = "Options"
+            optionsWindow?.isReleasedWhenClosed = false
+            optionsWindow?.delegate = self
         }
+        optionsWindow?.makeKeyAndOrderFront(nil)
     }
     
     func windowWillClose(_ notification: Notification) {
-        optionsWindow = nil // Release the window when it's closed
+        optionsWindow = nil
     }
 }
