b8f8d1b217dbc3eced828a920ff01b1d2223bf0d
Hazuki-295 <luosheng233@outlook.com>
2024-06-13 03:43:23 +0800

    Add descriptive elements to NlpAnnotatorsView

--- EudicNotes/Views/NlpAnnotatorsView.swift
+++ EudicNotes/Views/NlpAnnotatorsView.swift
@@ -85,6 +85,29 @@ class ServerStatusViewModel: ObservableObject {
     }
 }
 
+extension Color {
+    init(hexString: String, opacity: Double = 1.0) {
+        // Remove the hash if it exists
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        // Ensure it's valid
+        guard hex.count == 6 else {
+            self.init(.sRGB, red: 0, green: 0, blue: 0, opacity: opacity)
+            return
+        }
+        
+        // Convert to integer
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let red = Double((int & 0xff0000) >> 16) / 255.0
+        let green = Double((int & 0xff00) >> 8) / 255.0
+        let blue = Double(int & 0xff) / 255.0
+        
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
+
 // Main view managing NLP annotations
 struct NlpAnnotatorsView: View {
     @State private var input: String = "The big zucchini in the freezer will be shredded for bread."
@@ -93,9 +116,9 @@ struct NlpAnnotatorsView: View {
     @State private var hasAppeared: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 15) {
+        VStack(alignment: .leading) {
             HStack {
-                Label("Text to annotate:", systemImage: "square.and.pencil")
+                Label("Text to annotate:", systemImage: "book")
                 TextField("Enter input", text: $input)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .onSubmit {
@@ -110,8 +133,25 @@ struct NlpAnnotatorsView: View {
                 }
             }
             
-            NLPView(htmlContent: corenlpHTML)
-            NLPView(htmlContent: spacyHTML)
+            VStack(alignment: .leading) {
+                HStack {
+                    Label("Stanford CoreNLP", systemImage: "note.text").foregroundColor(Color(hexString: "#aa1d36"))
+                    Spacer()
+                    Image("corenlp").resizable().scaledToFit().background().border(Color.gray)
+                }
+                .frame(height: 30)
+                NLPView(htmlContent: corenlpHTML)
+            }
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Label("spaCy Dependency", systemImage: "note.text.badge.plus").foregroundColor(Color(hexString: "#0072cf"))
+                    Spacer()
+                    Image("spacy").resizable().scaledToFit()
+                }
+                .frame(height: 30)
+                NLPView(htmlContent: spacyHTML)
+            }
         }
         .padding(.top, 5)
         .padding(.bottom)
